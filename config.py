5fc259b4b095ed4b578875a94cc3fbc4909b08d5
Landon Miller <44953936+Millerlm012@users.noreply.github.com>
2019-11-30 20:48:09 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# configuration keys for the alpaca api --- May need to generate new keys --- if you do generate them and input new ones below
+# ---- is it possible to use requests to go to apalca api and generate new keys for us and put them below? ----
+
+ENDPOINT = 'https://paper-api.alpaca.markets'
+API_KEY = 'PKU5US4MOZMX02FUSJMI'
+SECRET_KEY = 'LUfXFSXXvk601vaKn3fCcv0NTT3Nh0Ey6O3IKAYf'
+
---
+++ stock_scanner.py
@@ -0,0 +1,2 @@
+# GOAL = write algorithm to find stocks following the 3 bar play
+
