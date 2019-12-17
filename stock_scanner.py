0c85d3356e35395024bf25c48e163ab63c4f6d0b
Landon Miller <44953936+Millerlm012@users.noreply.github.com>
2019-12-16 19:31:06 -0600

    Add files via upload

--- stock_scanner.py
+++ stock_scanner.py
@@ -4,19 +4,41 @@
 import os
 
 from iexfinance.stocks import Stock
-from iexfinance.stocks import get_historical_data
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
-from iexfinance.stocks import get_earnings_today
-from iexfinance.stocks import get_historical_data
 from config import *
 
 # setting iex api version to test [sandbox] - use sandbox api keys
 os.environ['IEX_API_VERSION'] = 'iexcloud-sandbox'
 os.environ['IEX_TOKEN'] = IEX_API_KEY
 
-amzn = Stock('AMZN')
-amzn_balance = amzn.get_balance_sheet()
+# do I need the api key for actual requests?
 
-print(amzn_balance)
+# Returning amzn balance
+amzn = Stock('AMZN')
+amzn_balance_lst = []
+amzn_balance_lst = amzn.get_balance_sheet()
+
+amzn_balance_df = pd.DataFrame(amzn_balance_lst)
+print(amzn_balance_df)
+
+# keep the output separated for now
+print(" ")
+print("--------------------------------------------------------------------- ")
+print(" ")
+
+
+# get historical prices of stock FOR THE PAST MONTH - assuming use batch to return multiple
+# put historical_prices into pandas dataframe
+amzn_hist_lst = []
+amzn_hist_lst = amzn.get_historical_prices()
+
+# making list into dataframe - (df = dataframe)
+amzn_hist_df = pd.DataFrame(amzn_hist_lst)
+print(amzn_hist_df)
+
+
+# returns amzn real time price/quote - for multiple symbols use tuple/batch.get_price()
+amzn_price = amzn.get_price()
+print(amzn_price)
