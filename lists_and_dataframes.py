ced4c0ce46535483175e88bbf9afb5ec767c62d6
Landon Miller <44953936+Millerlm012@users.noreply.github.com>
2020-03-09 22:46:40 -0500

    Update lists_and_dataframes.py

--- lists_and_dataframes.py
+++ lists_and_dataframes.py
@@ -1,18 +1,64 @@
+import pandas as pd
+
 # testing list of lists and pd dataframes
 
 """
 Using pandas dataframe to reorganize the data collected throughout the day
 """
-import pandas as pd
 
-# each new list will have 3 values --> [0] = high , [1] = low , [2] = price
-test = [[] for i in range(361)]
-test[1].append(3)
-test[1].append(4)
-test[1].append(5)
+all_stock_data = {'min_0': 'N/A'}
+stock_data_keys = {
+    'high': '',
+    'low': '',
+    'dif.': '',
+    'price': '',
+    'percent': '',
+    'avg.': '',
+    'percent_price': '',
+    'stop_loss': '',
+    'entry_price': '',
+}
 
-print(test)
 
-df_test = pd.DataFrame(test, columns=['high', 'low', 'price'])
+# making all of the keys for the all_stock_data dictionary
+y = 1
+for i in range(361):
+    all_stock_data['min_' + str(y)] = {'high': '', 'low': '', 'dif.': '', 'price': '', 'percent': '', 'avg.': '', 'percent_price': '', 'stop_loss': '', 'entry_price': ''}
+    y += 1
 
-print(df_test)
+minute_ticker = 5
+all_stock_data['min_' + str(minute_ticker)]['dif.'] = str(.06)
+all_stock_data['min_' + str(minute_ticker - 4)]['dif.'] = str(.05)
+all_stock_data['min_' + str(minute_ticker - 3)]['dif.'] = str(.04)
+all_stock_data['min_' + str(minute_ticker - 2)]['dif.'] = str(.03)
+all_stock_data['min_' + str(minute_ticker - 1)]['dif.'] = str(.02)
+
+minute_5 = all_stock_data['min_' + str(minute_ticker)]['dif.'] = str(.06)
+minute_4 = all_stock_data['min_' + str(minute_ticker - 4)]['dif.'] = str(.05)
+minute_3 = all_stock_data['min_' + str(minute_ticker - 3)]['dif.'] = str(.04)
+minute_2 = all_stock_data['min_' + str(minute_ticker - 2)]['dif.'] = str(.03)
+minute_1 = all_stock_data['min_' + str(minute_ticker - 1)]['dif.'] = str(.02)
+
+
+sum_of_5 = minute_5 + minute_4 + minute_3 + minute_2 + minute_1
+average = sum_of_5 * int(.2)
+print(average)
+print(sum_of_5)
+
+
+all_stock_data['min_3']['avg.'] = str(average)
+
+
+print(all_stock_data)
+
+all_stock_data['min_' + str(minute_ticker)]['high'] = 25
+all_stock_data['min_' + str(minute_ticker + 1)]['high'] = 36
+all_stock_data['min_1']['price'] = 26.90
+all_stock_data['min_4']['low'] = .05
+
+print(all_stock_data)
+
+
+df = pd.DataFrame(all_stock_data).swapaxes('index', 'columns')
+
+print(df)
